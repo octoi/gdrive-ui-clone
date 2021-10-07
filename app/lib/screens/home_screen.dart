573d3bf8632716c5a9e0ee1f450fb7d00f820eb3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-07 15:55:00 +0530

    search bar

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,80 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appBgColor,
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(100.0),
+      //   child: AppBar(
+      //     backgroundColor: appBgColor,
+      //     title: TextField(
+      //       decoration: InputDecoration(
+      //         contentPadding: const EdgeInsets.all(20.0),
+      //         border: OutlineInputBorder(
+      //           borderRadius: BorderRadius.circular(50.0),
+      //           borderSide: BorderSide.none,
+      //         ),
+      //         fillColor: appSecondaryColor,
+      //         filled: true,
+      //         hintStyle: TextStyle(color: Colors.grey[800]),
+      //         hintText: "Search in Drive",
+      //         prefixIcon: const Icon(
+      //           Icons.menu,
+      //           color: appTextColor,
+      //           size: 28.0,
+      //         ),
+      //         suffix: CircleAvatar(
+      //           radius: 20.0,
+      //           backgroundImage: NetworkImage(rickImage),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      //),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.symmetric(
+              vertical: 5.0,
+              horizontal: 20.0,
+            ),
+            decoration: BoxDecoration(
+              color: appSecondaryColor,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(
+                  Icons.menu,
+                  size: 26.0,
+                ),
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                      ),
+                      fillColor: appSecondaryColor,
+                      filled: true,
+                      hintStyle: TextStyle(color: Colors.grey[800]),
+                      hintText: "Search in Drive",
+                    ),
+                  ),
+                ),
+                const CircleAvatar(
+                  radius: 18.0,
+                  backgroundImage: NetworkImage(rickImage),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
